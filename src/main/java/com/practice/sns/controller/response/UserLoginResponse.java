936df95a4b5a34bee3e2cb964346d2670a994392
kimdev <kimdev1242@gmail.com>
2023-11-07 19:24:47 +0900

    SNS 기능 구현
    기본 SNS 기능 구현

---
+++ src/main/java/com/practice/sns/controller/response/UserLoginResponse.java
@@ -0,0 +1,10 @@
+package com.practice.sns.controller.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserLoginResponse {
+    private String token;
+}
---
+++ src/main/java/com/practice/sns/model/UserRole.java
@@ -0,0 +1,7 @@
+package com.practice.sns.model;
+
+public enum UserRole {
+
+    ADMIN,
+    USER
+}
